28c0309a0db1a40dff174e964fc4856a872df0ad
Pratibhapa <pratibhaa4321@gmail.com>
2023-10-19 10:07:02 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [openNav, setOpenNav] = useState(false);
+  const [subData, setSubData] = useState({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header setOpenNav={setOpenNav} openNav={openNav} setSubData= {setSubData} subData={subData}/>
+
+      <div className='cards'>
+        {
+         carts.map((item, index) => (
+          <cartItem key={index} data= {item} setSubData= {setSubData} subData={subData}/>
+         ))
+        }
+      </div>
     </div>
-  );
+  ); 
+}
+
+const Nav = (props)=> {
+  const [totalPrice, setTotalPrice] = useState(0);
+  useEffect (() => {
+    var bumpTotal = 0;
+    Object.keys(subData).map((key) => {
+      if (subData[key]["count"] <= 0) {
+        let bump = {...subData};
+        delete bump [key];
+        setSubData(bump);
+      }
+      
+    })
+  })
+
 }
 
 export default App;
